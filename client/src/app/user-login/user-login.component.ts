832fa311fc59e8a1f34f802123542649edb42899
Marc-Antoine Jacob <marc-antoine-2.jacob@polymtl.ca>
2019-01-16 18:42:07 -0500

    [1-DEV-MAJ] Ajout de constraintes pour le nom d'utilisateur (bordure rouge si erroné)

--- client/src/app/user-login/user-login.component.ts
+++ client/src/app/user-login/user-login.component.ts
@@ -7,4 +7,13 @@ import { Component } from "@angular/core";
 })
 export class UserLoginComponent {
   public username: string;
+  public usernameValid: boolean = true;
+  private readonly usernameMinLength: number = 3;
+
+  public validateUsername(u: string): void {
+    const regex: RegExp = /^[0-9a-z]+$/i;
+
+    this.username = u;
+    this.usernameValid = regex.test(u) && u.length >= this.usernameMinLength;
+  }
 }
