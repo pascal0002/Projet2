556e442681d76ea9cb8254611abc1d77b6f75aff
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-01-17 16:20:41 -0500

    [2-DEV-PAM] For loop fonctionnel

--- client/src/app/fiche-pdvsimple/liste-fiche-pdvsimple.ts
+++ client/src/app/fiche-pdvsimple/liste-fiche-pdvsimple.ts
@@ -1,8 +1,8 @@
-import {fichePDVSimple12} from './fiche-pdvsimple';
+import {fichePDVSimple} from './fiche-pdvsimple';
 
-export const LISTE_FICHE_PDV_SIMPLE: fichePDVSimple12[] =[
+export const LISTE_FICHE_PDV_SIMPLE: fichePDVSimple[] =[
     {   titre: "Nissan", 
-        nomImage: 'nissan_patrol.jpg',
+        nomImage: 'assets/images/nissan_patrol.jpg',
         meilleurTempsSolo: ['1:21 Paul', '1:34 AAA', '1:45 OOO'],
         meilleurTemps1V1: ['1:21 Ko', '1:33 ferfw', '1:45 frew']
     }
