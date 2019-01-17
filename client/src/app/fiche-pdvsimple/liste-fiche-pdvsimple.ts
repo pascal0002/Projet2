cddc02336dc42e8c15900da212521c4b469b9ae5
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-01-17 16:39:39 -0500

    [2-TEST-PAM] Test ajout de fiches

--- client/src/app/fiche-pdvsimple/liste-fiche-pdvsimple.ts
+++ client/src/app/fiche-pdvsimple/liste-fiche-pdvsimple.ts
@@ -5,6 +5,12 @@ export const LISTE_FICHE_PDV_SIMPLE: fichePDVSimple[] =[
         nomImage: 'assets/images/nissan_patrol.jpg',
         meilleurTempsSolo: ['1:21 Paul', '1:34 AAA', '1:45 OOO'],
         meilleurTemps1V1: ['1:21 Ko', '1:33 ferfw', '1:45 frew']
+    },
+    {   titre: "test", 
+      nomImage: 'assets/images/TEST.jpg',
+    meilleurTempsSolo: ['1:21 Paul', '1:34 AAA', '1:45 OOO'],
+    meilleurTemps1V1: ['1:21 Ko', '1:33 ferfw', '1:45 frew']
+
     }
 
 ];
