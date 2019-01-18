bd556bfb3c3aed9fbfb2db92abeec2d1134edbdd
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-01-17 20:13:54 -0500

    [2-DEV-PAM] correction des temps

--- client/src/app/fiche-pdvsimple/mock-liste-fiche-pdvsimple.ts
+++ client/src/app/fiche-pdvsimple/mock-liste-fiche-pdvsimple.ts
@@ -4,13 +4,13 @@ export const LISTE_FICHE_PDV_SIMPLE: fiche[] =[
     {   titre: "Nissan", 
         nomImage: 'assets/images/nissan_patrol.jpg',
         meilleurTempsSolo: ['1:21 Paul', '1:34 AAA', '1:45 OOO'],
-        meilleurTemps1V1: ['1:21 Ko', '1:33 ferfw', '1:45 frew']
+        meilleurTemps1V1: ['0:58 Ko', '1:13 frew1', '1:21 frew2']
     },
 
     {   titre: "test", 
         nomImage: 'assets/images/TEST.jpg',
-        meilleurTempsSolo: ['1:21 Paul', '1:34 AAA', '1:45 OOO'],
-        meilleurTemps1V1: ['1:21 Ko', '1:33 ferfw', '1:45 frew']
+        meilleurTempsSolo: ['3:30 blabla', '4:31 user', '4:55 name'],
+        meilleurTemps1V1: ['2:08 nick', '2:33 te', '2:48 nice']
 
     }
 
