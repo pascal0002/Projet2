ac616b414f244348395f308be5c244380fac7deb
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-01-19 15:06:24 -0500

    [DEV-PAM] retour en arriere dev

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,38 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from '@angular/forms'; 
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
 
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
-import { HttpClientModule } from "@angular/common/http";
-import { PartsListViewComponent } from './parts-list-view/parts-list-view.component';
-import { TwoDimensionGameCardComponent } from './2d-game-card/2d-game-card.component';
-import { ThreeDimensionGameCardComponent } from './3d-game-card/3d-game-card.component';
-import { GenericGameCardComponent } from './generic-game-card/generic-game-card.component';
-import { AdministrationViewComponent } from "./administration-view/administration-view.component";
 
- const appRoutes: Routes =[
-   { path: 'admin', component: AdministrationViewComponent },
-   { path: '', component: PartsListViewComponent }
- ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PartsListViewComponent,
-    TwoDimensionGameCardComponent,
-    ThreeDimensionGameCardComponent,
-    GenericGameCardComponent,
-    AdministrationViewComponent
-  ],
+      ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [BasicService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
