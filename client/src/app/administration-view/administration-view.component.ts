77298e037e1c2357f1d6a291c6d522023f68e26d
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-01-19 17:47:25 -0500

    [3-DEV-PAM] Travail sur le css et html de la vue admin

--- client/src/app/administration-view/administration-view.component.ts
+++ client/src/app/administration-view/administration-view.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {THREE_DIMENSION_GAME_CARD_LIST} from '../3d-game-card/3d-game-card-mock-list';
+import {TWO_DIMENSION_GAME_CARD_LIST} from '../2d-game-card/2d-game-card-mock-list';
+
 
 @Component({
   selector: 'app-administration-view',
   templateUrl: './administration-view.component.html',
-  styleUrls: ['./administration-view.component.css']
+  styleUrls: [
+              './administration-view.component.css',
+              '../generic-game-card/generic-game-card.component.css',
+              '../parts-list-view/parts-list-view.component.css'
+             ]
 })
 export class AdministrationViewComponent implements OnInit {
 
   constructor() { }
+  //TODO: Subscribe the lists to an observer so that the view is updated when 
+  //      lists are deleted/created.
+
+  twoDimensionGameCardListAdmin = TWO_DIMENSION_GAME_CARD_LIST;
+  threeDimensionGameCardListAdmin = THREE_DIMENSION_GAME_CARD_LIST;
+
+  
 
   ngOnInit() {
   }
