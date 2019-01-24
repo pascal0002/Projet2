e0212911ef01d0e55289f77c70811577e85bbe2a
Pascale Sylvestre <pascale_sy@hotmail.com>
2019-01-24 18:28:08 -0500

    [1.REVISION.PS] - websocket plus generique

--- client/src/app/websocket.service.ts
+++ client/src/app/websocket.service.ts
@@ -11,14 +11,19 @@ export class WebsocketService {
     public initSocket(): void {
         this.socket = socketIo(this.BASE_URL);
     }
-
+    /*
+    TODO : modifier le websocket pour qu'il soit generique
+    public sendMessage(messageType: MessageType, message: string): void {
+      this.socket.emit(messageType, message);
+    }
+*/
     public testUsername(username: string): void {
         this.socket.emit("testUsername", username);
     }
 
     public connectUsername(username: string): void {
         this.socket.emit("connectUsername", username);
-  }
+    }
 
     public onTestUsername(): Observable<boolean> {
         return new Observable<boolean>((observer) => {
