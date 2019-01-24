ff79cfe39ef33e26c28a700c094776e6b2c8d068
ibcho <mateymonkey@gmail.com>
2019-01-24 17:59:21 -0500

    [6-DEV-IC] creation service pour generation image de differences

--- client/src/app/game-card-form-2d/game-card-form-2d.component.ts
+++ client/src/app/game-card-form-2d/game-card-form-2d.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import {FormHandler2dService} from "./form-handler-2d.service";
+import {DifferencesGeneratorService} from "../differences-generator.service";
 
 @Component({
   selector: "app-game-card-form-2d",
@@ -10,10 +11,13 @@ export class GameCardFormComponent implements OnInit {
   public isFk: boolean = true;
   public title: string;
 
-  public constructor(private formHandlerService: FormHandler2dService) { }
+  public constructor(private formHandlerService: FormHandler2dService, private differencesGenerator: DifferencesGeneratorService) { }
 
   public closeForm2D(): void {
     this.formHandlerService.closeForm();
+  }
+  public generateDifferences(): void {
+    //this.differencesGenerator.generateDifferences();
   }
   public ngOnInit(): void {
     /**/
