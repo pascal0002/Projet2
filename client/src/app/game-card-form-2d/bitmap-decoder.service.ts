16cc8b7ed2216a00f7024bb2f4903c079f2f2250
Pascale Sylvestre <pascale_sy@hotmail.com>
2019-01-28 12:55:02 -0500

    [1.DEV.PS] ajoue des pixel a la structure json

--- client/src/app/game-card-form-2d/bitmap-decoder.service.ts
+++ client/src/app/game-card-form-2d/bitmap-decoder.service.ts
@@ -4,17 +4,18 @@ import { BitmapImage } from "../../../../common/communication/BitmapImage";
 const WIDTH_OFFSET: number = 18;
 const HEIGHT_OFFSET: number = 22;
 const BITS_PER_PIXEL_OFFSET: number = 28;
-//const PIXEL_OFFSET: number = 10;
+const PIXEL_OFFSET: number = 10;
 
 @Injectable()
 export class BitmapDecoderService {
 
   public decodeBitmapFile(file: File): BitmapImage {
-    
+
     const bitmapImage: BitmapImage = { height: 0, width: 0, bitDepth: 0, fileName: "", pixels: [] };
     bitmapImage.fileName = file.name;
     let bmpPixelsBuffer: ArrayBuffer = new ArrayBuffer(file.size);
     const fileReader: FileReader = new FileReader();
+
     fileReader.onload = () => {
 
       bmpPixelsBuffer = fileReader.result as ArrayBuffer;
@@ -23,9 +24,12 @@ export class BitmapDecoderService {
       bitmapImage.width = dataView.getUint32(WIDTH_OFFSET, true);
       bitmapImage.height = dataView.getUint32(HEIGHT_OFFSET, true);
       bitmapImage.bitDepth = dataView.getUint32(BITS_PER_PIXEL_OFFSET, true);
-
+      const offset: number = dataView.getUint32(PIXEL_OFFSET, true);
+      bitmapImage.pixels = Array.from(new Uint8Array(bmpPixelsBuffer, offset));
     };
+
     fileReader.readAsArrayBuffer(file);
+
     return bitmapImage;
   }
 }
