70ae9a4c2714ec2db71bdb7b2cf1bb015d562d87
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-02-02 16:54:04 -0500

    [6-REV-PAM] Correction de TSLint pour le service de générateur de BMP.

--- server/app/services/bmp-file-generator.service.ts
+++ server/app/services/bmp-file-generator.service.ts
@@ -1,30 +1,30 @@
-import { injectable, inject } from "inversify";
+import * as fs from "fs";
+import { inject, injectable } from "inversify";
 import "reflect-metadata";
 import { IBitmapImage } from "../../../common/communication/BitmapImage";
-import * as fs from "fs";
-import { BitmapEncoder } from "./bitmap-encoder.service";
-import Types from "../types";
 import { IFormInfo } from "../../../common/communication/FormInfo";
+import Types from "../types";
+import { BitmapEncoder } from "./bitmap-encoder.service";
 
 @injectable()
 export class BmpFileGenerator {
-    constructor(@inject(Types.BitmapEncoder) private bitmapEncoderService: BitmapEncoder){}
-    
-    generateBMPFiles(form: IFormInfo, imageOfDifferences: IBitmapImage): void {
+    public constructor(@inject(Types.BitmapEncoder) private bitmapEncoderService: BitmapEncoder) {}
+
+    public generateBMPFiles(form: IFormInfo, imageOfDifferences: IBitmapImage): void {
         this.generateOriginalBMPFile(form.originalImage);
         this.generateModifedBMPFile(form.modifiedImage);
         this.generateDifferenceBMPFile(imageOfDifferences);
     }
 
-    generateOriginalBMPFile(image: IBitmapImage): void {
+    public generateOriginalBMPFile(image: IBitmapImage): void {
         fs.writeFileSync(process.cwd() + "/public/originalImages/" + image.fileName, this.bitmapEncoderService.encodeBitmap(image));
     }
 
-    generateModifedBMPFile(image: IBitmapImage): void {
+    public generateModifedBMPFile(image: IBitmapImage): void {
         fs.writeFileSync(process.cwd() + "/public/modifiedImages/" + image.fileName, this.bitmapEncoderService.encodeBitmap(image));
     }
 
-    generateDifferenceBMPFile(image: IBitmapImage): void {
+    public generateDifferenceBMPFile(image: IBitmapImage): void {
         fs.writeFileSync(process.cwd() + "/public/differenceImages/" + image.fileName, this.bitmapEncoderService.encodeBitmap(image));
     }
 }
