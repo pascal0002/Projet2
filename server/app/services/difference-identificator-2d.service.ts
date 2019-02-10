53656112a4cb537fd3759f8e4d10e0d67391321b
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-02-10 16:45:34 -0500

    [4-DEV-PAM] Capable d'aller chercher le pixel à la position clické à partir du tableau 1D de pixels.

--- server/app/services/difference-identificator-2d.service.ts
+++ server/app/services/difference-identificator-2d.service.ts
@@ -1,7 +1,9 @@
 import { injectable } from "inversify";
 import { IBitmapImage } from "../../../common/communication/BitmapImage";
 import { IClickCoordinates } from "../../../common/communication/ClickCoordinates";
-//import { ModifiedImg } from "../../mock/image-mock";
+import { ServerConstants } from "../../../common/communication/Constants";
+import { IPixel } from "../../../common/communication/Pixel";
+import { ModifiedImg } from "../../mock/image-mock";
 
 @injectable()
 export class DifferenceIdentificator2DService {
@@ -14,11 +16,32 @@ export class DifferenceIdentificator2DService {
     public confirmDifference(clickPosition: IClickCoordinates, differenceImage: IBitmapImage, modifiedImage: IBitmapImage): boolean {
 
         // Where the pixels of the image of differences are
-        // const imgOfDifference: ModifiedImg = new ModifiedImg();
+        const imgOfDifference: ModifiedImg = new ModifiedImg();
 
         console.log("Pos X: " + clickPosition.xPos);
         console.log("Pos Y: " + clickPosition.yPos);
 
+        console.log(this.getPixelAtPos(clickPosition, imgOfDifference.pixels));
+        console.log(imgOfDifference.pixels[1919]);
+
+
+
         return true;
     }
+
+    public getPixelAtPos(clickPosition: IClickCoordinates, pixelArray: number[]): IPixel {
+        const POS_IN_ARRAY: number = this.getPositionInArray(clickPosition);
+        console.log(POS_IN_ARRAY);
+        return {
+            red: pixelArray[POS_IN_ARRAY],
+            green: pixelArray[POS_IN_ARRAY + 1],
+            blue: pixelArray[POS_IN_ARRAY + 1 + 1],
+        };
+    }
+
+    public getPositionInArray(clickPosition: IClickCoordinates): number {
+        return ((clickPosition.yPos * ServerConstants.ACCEPTED_WIDTH * 3)
+                + clickPosition.xPos * ServerConstants.BYTES_PER_PIXEL);
+    }
+
 }
