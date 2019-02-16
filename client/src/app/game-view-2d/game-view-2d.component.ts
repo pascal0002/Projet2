dbc76cbb3dbeb8f261f4c00f853736f71767f95a
ibcho <mateymonkey@gmail.com>
2019-02-16 18:22:53 -0500

    [4.DEV-IC] L'image dessinée sur le canvas possède les bonnes couleurs RGBA

--- client/src/app/game-view-2d/game-view-2d.component.ts
+++ client/src/app/game-view-2d/game-view-2d.component.ts
@@ -4,7 +4,6 @@ import { Router } from "@angular/router";
 import { ClientConstants, ServerConstants } from "../../../../common/communication/Constants";
 import { IImagePath } from "../../../../common/communication/ImagePath";
 import { GameCard } from "../../../../common/communication/game-card";
-import { TWO_DIMENSION_GAME_CARD_LIST } from "../../../../server/public/mock/2d-game-card-mock-list";
 
 @Component({
   selector: "app-game-view-2d",
@@ -21,7 +20,6 @@ export class GameView2DComponent implements AfterViewInit {
   public originalImagePixels: number[] = [];
 
   public constructor(private http: HttpClient, route: Router) {
-    this.game2d = TWO_DIMENSION_GAME_CARD_LIST[0];
   }
 
   // Peut etre ajouter un attribut dans gameCard au lieu de faire ca
@@ -39,56 +37,18 @@ export class GameView2DComponent implements AfterViewInit {
 
   public ngAfterViewInit(): void {
 
-    // Create img
-    // const img: HTMLImageElement = new Image();
-    // img.src = this.game2d.imageName;
-
     // Create canvas
     const canvas: HTMLCanvasElement = document.createElement("canvas");
     canvas.width = 640;
     canvas.height = 480;
     document.body.appendChild(canvas);
     this.ctx = canvas.getContext("2d");
-    const imageData: ImageData = new ImageData(640, 480);
 
-    if (this.ctx) {
-      // Iterate through every pixel
-      this.originalImage()
-      .then((res) => {
-        console.log(res);
-        this.originalImagePixels = res;
-        let j: number = 0;
-        for (let i: number = 0; i < this.originalImagePixels.length; i += 3) {
-          imageData.data[j + 0] = this.originalImagePixels[i + 0];    // R value
-          imageData.data[j + 1] = this.originalImagePixels[i + 1];    // G value
-          imageData.data[j + 2] = this.originalImagePixels[i + 2];    // B value
-          imageData.data[j + 3] = 255;                                // A value
-          j += 4;
-        }
-        if (this.ctx) {
-          this.ctx.putImageData(imageData, 0, 0, );
-        }
-      });
-      /*for (let i: number = 0; i < this.originalImage.length; i ++) {
-        image[i] = this.originalImage()[i];
-      }*/
-      /*for (let i: number = 0; i < this.originalImagePixels.length; i += 4) {
-        imageData.data[i + 0] = this.originalImagePixels[i + 0];    // R value
-        imageData.data[i + 1] = this.originalImagePixels[i + 1];    // G value
-        imageData.data[i + 2] = this.originalImagePixels[i + 2];    // B value
-        imageData.data[i + 3] = 255;             // A value
-      }
-      this.ctx.putImageData(imageData, 0, 0, );*/
-    }
-
-    // Load image, create canvas and draw
-
-    // this.ctx.drawImage(img, 0, 0, img.width, img.height);
-
-    // addEventListener("click", (e) => {
-    //      this.clickImage(e);
-    //      this.getPixel();
-    //    });
+    this.drawPixelsInCanvas();
+    addEventListener("click", (e) => {
+      this.clickImage(e);
+      this.getPixel();
+    });
   }
 
   public async originalImage(): Promise<number[]> {
@@ -96,60 +56,49 @@ export class GameView2DComponent implements AfterViewInit {
     return new Promise<number[]> ((resolve: Function, res: Function) =>
     resolve(this.http.post<number[]>(ClientConstants.SERVER_BASE_URL + "api/differences/bitmap_encoder", this.imagePath)
     .toPromise()));
-    /*
-    .then(
-      (res) => {
-        this.originalImagePixels = res;
-      },
-    )
-    .catch(
-      (err) => {console.error("erreur :", err); },
-    );*/
-
   }
 
+  public drawPixelsInCanvas(): void {
+    const imageData: ImageData = new ImageData(640, 480);
+    if (this.ctx) {
+      // Iterate through every pixel
+      this.originalImage()
+      .then((res) => {
+        this.originalImagePixels = res;
+        let j: number = 0;
+        for (let i: number = 0; i < this.originalImagePixels.length; i += 3) {
+          imageData.data[j + 0] = this.originalImagePixels[i + 2];    // B value
+          imageData.data[j + 1] = this.originalImagePixels[i + 1];    // G value
+          imageData.data[j + 2] = this.originalImagePixels[i + 0];    // R value
+          imageData.data[j + 3] = 255;                                // A value
+          j += 4;
+        }
 
+        if (this.ctx) {
+          this.ctx.putImageData(imageData, 0, 0, );
+        }
 
-
-
-
-
+      });
+    }
+  }
 
   public clickImage(event: MouseEvent): void {
     this.clickPosition = [event.offsetX, event.offsetY];
-    this.sendClickPosition(this.clickPosition);
+    // this.sendClickPosition(this.clickPosition);
   }
 
   public getPixel(): void {
 
-    this.http.post<string>(ClientConstants.SERVER_BASE_URL + "api/differences/bitmap_encoder", this.imagePath)
-    .toPromise()
-    .then(
-      (res) => {
-        console.log(res);
-      },
-    )
-    .catch(
-      (err) => {console.error("erreur :", err); },
-    );
-
-    if (this.ctx) {
-      // const imageData: ImageData = this.ctx.getImageData(xPos, yPos, img.width, img.height);
-      // console.log(imageData.data.length);
-      // console.log("First pixel colors: " + imageData.data);
-    }
-    // const imageData: ImageData = new ImageData(img.width, img.height);
-
-    // return
   }
 
+  /*
   public sendClickPosition(mousePos: Array<number>): void {
-  //   this.http.post<Array<number>>(ClientConstants.SERVER_BASE_URL + "api/differences/difference_validator`, mousePos)
-  //   .toPromise()
-  //   .then()
-  //   .catch(
-  //     (err) => {console.error("erreur :", err); },
-  //   );
-  }
+     this.http.post<Array<number>>(ClientConstants.SERVER_BASE_URL + "api/differences/difference_validator", mousePos)
+     .toPromise()
+     .then()
+     .catch(
+       (err) => {console.error("erreur :", err); },
+     );
+  }*/
 
 }
