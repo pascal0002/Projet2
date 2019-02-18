918c16e4513c44ae00179b700f7eebf315f0bc3b
Pascale Sylvestre <pascale_sy@hotmail.com>
2019-02-18 17:26:51 -0500

    [DEV-PS] ajoue tu test pour la nom unique des carte 2D

--- server/app/controllers/game-cards.controller.ts
+++ server/app/controllers/game-cards.controller.ts
@@ -2,10 +2,12 @@ import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
 import * as mongoose from "mongoose";
 import { IBitmapImage } from "../../../common/communication/BitmapImage";
-import { Dimension, Constants } from "../../../common/communication/Constants";
+import { Constants, Dimension } from "../../../common/communication/Constants";
 import { BmpFileGenerator } from "../services/bmp-file-generator.service";
+import { DatabaseService } from "../services/database.service";
 import { FormValidator2DService } from "../services/form-validator-2D.service";
 import { FormValidator3DService } from "../services/form-validator-3D.service";
+import { gameCard2D } from "../services/game-card-2D-schema";
 import { GameCardsService } from "../services/game-cards.service";
 import Types from "../types";
 
@@ -15,7 +17,8 @@ export class GameCardsController {
     public constructor(@inject(Types.FormValidator2DService) private formValidator2DService: FormValidator2DService,
                        @inject(Types.FormValidator3DService) private formValidator3DService: FormValidator3DService,
                        @inject(Types.GameCardsService) private gameCardsService: GameCardsService,
-                       @inject(Types.BmpFileGenerator) private bmpFileGeneratorService: BmpFileGenerator) { }
+                       @inject(Types.BmpFileGenerator) private bmpFileGeneratorService: BmpFileGenerator,
+                       @inject(Types.DatabaseService) private databaseService: DatabaseService) { }
 
     public get router(): Router {
         const router: Router = Router();
@@ -40,15 +43,21 @@ export class GameCardsController {
             if (!this.formValidator2DService.validateForm(req.body)) {
                 res.status(Constants.ERROR).send("Les informations envoyées ne sont pas valides!");
             }
-
-            this.gameCardsService.generateDifferences(req.body.originalImage, req.body.modifiedImage)
-            .then((image: IBitmapImage) => {
-                if (this.gameCardsService.validateDifferencesImage(image)) {
-                    this.bmpFileGeneratorService.generateBMPFiles(req.body, image);
-                    res.json(this.gameCardsService.addGameCard2D(req.body, image));
-                } else {
-                    res.status(Constants.ERROR).send("Les deux images sélectionnées doivent avoir exactement 7 différences");
+            this.databaseService.getOne(gameCard2D, {title : req.body.gameName})
+            .then((gamecard: mongoose.Document) => {
+                if (gamecard) {
+                    res.status(Constants.ERROR).send("Une partie avec ce nom existe déjà !");
                 }
+                this.gameCardsService.generateDifferences(req.body.originalImage, req.body.modifiedImage)
+                .then((image: IBitmapImage) => {
+                    if (this.gameCardsService.validateDifferencesImage(image)) {
+                        this.bmpFileGeneratorService.generateBMPFiles(req.body, image);
+                        res.json(this.gameCardsService.addGameCard2D(req.body, image));
+                    } else {
+                        res.status(Constants.ERROR).send("Les deux images sélectionnées doivent avoir exactement 7 différences");
+                    }
+                })
+               .catch((err: Error) => console.error(err));
             })
             .catch((err: Error) => console.error(err));
         });
