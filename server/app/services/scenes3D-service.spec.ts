4622b7aae427299770e82121c33a69867f947f09
Pascale Sylvestre <pascale_sy@hotmail.com>
2019-02-18 12:12:00 -0500

    [9.TEST-PS] mock database service pour les test de scenes3D

--- server/app/services/scenes3D-service.spec.ts
+++ server/app/services/scenes3D-service.spec.ts
@@ -1,6 +1,10 @@
 // tslint:disable:no-any
 // tslint:disable:no-magic-numbers
 import * as mongoose from "mongoose";
+import * as sinon from "sinon";
+import { Scene3DService } from "./scenes3D-service";
+
+let scenes3DService: Scene3DService;
 
 class MockDatabaseService {
 
@@ -28,3 +32,24 @@ class MockDatabaseService {
         return Promise.resolve(0);
     }
 }
+
+const databaseService: any = new MockDatabaseService();
+let databaseServiceStub: sinon.SinonStub;
+
+describe("game-cards-service", () => {
+
+  describe("addScene3D", () => {
+
+    beforeEach((done: Mocha.Done) => {
+      scenes3DService = new Scene3DService(databaseService);
+      databaseServiceStub = sinon.stub(databaseService, "add");
+      done();
+    });
+
+    afterEach((done: Mocha.Done) => {
+      databaseServiceStub.restore();
+      done();
+    });
+  });
+
+});
