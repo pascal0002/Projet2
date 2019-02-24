5b38073fc2b3338a200a339e3434aaaf6698adc3
Marc-Antoine Jacob <marc-antoine-2.jacob@polymtl.ca>
2019-02-24 09:53:28 -0500

    [TEST-MAJ] Ajout du fichier de test pour game-view service et initialisation

--- client/src/app/game-view/game-view.service.spec.ts
+++ client/src/app/game-view/game-view.service.spec.ts
@@ -1,16 +1,21 @@
+import { Dimension } from "../../../../common/communication/Constants";
+import { GameCard } from "../../../../common/communication/game-card";
 import { GameViewService } from "./game-view.service";
 
-
 let service: GameViewService;
+const mockGameCard: GameCard = {
+  title: "Bonjour", image: "x.bmp", imageModified: "y.bmp",
+  bestTimeSolo: [{ user: "Ali", time: 20 }, { user: "Baba", time: 40 }, { user: "Michel", time: 133 }],
+  bestTime1v1: [{ user: "Haxer", time: 1 }, { user: "User231", time: 122 }, { user: "NickPacté", time: 9001 }],
+  dimension: Dimension.TWO_DIMENSION,
+};
 
 describe("GameViewService", () => {
 
   beforeEach(() => {
+    service.reset();
     service = new GameViewService();
-  });
-
-  it("should listen for username validation", () => {
-
+    service.gamecard = mockGameCard;
   });
 
 });
