3da72ec6dcaa1b0d460bad71cdfa535c0363c61b
Pascale Sylvestre <pascale_sy@hotmail.com>
2019-03-04 18:28:17 -0500

    [4.DEV-PS] ajoue de la reqete post

--- server/app/controllers/high-score.controller.ts
+++ server/app/controllers/high-score.controller.ts
@@ -1,5 +1,6 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { injectable } from "inversify";
+import { Constants } from "../../../common/communication/Constants";
 
 @injectable()
 export class HighScoreController {
@@ -8,6 +9,9 @@ export class HighScoreController {
 
     public get router(): Router {
         const router: Router = Router();
+        router.post(Constants.BACK_SLASH, (req: Request, res: Response, next: NextFunction) => {
+            res.json();
+        });
 
         return router;
     }
