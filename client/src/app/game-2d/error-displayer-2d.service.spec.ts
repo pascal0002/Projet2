7284674d31627c1830ba2b5ccfadfb206d6bb786
Pascal-Alexandre <pascalalexandre.morel@gmail.com>
2019-03-06 17:21:31 -0500

    [8-TEST-PAM] Écriture de quelques tests pour l'afficheur d'erreur 2D.

--- client/src/app/game-2d/error-displayer-2d.service.spec.ts
+++ client/src/app/game-2d/error-displayer-2d.service.spec.ts
@@ -1,5 +1,8 @@
-import { TestBed } from "@angular/core/testing";
+// tslint:disable:no-magic-numbers
 
+import { TestBed } from "@angular/core/testing";
+import { IClickInfo } from "../../../../common/communication/ClickInfo";
+import { Constants } from "../../../../common/communication/Constants";
 import { ErrorDisplayer2dService } from "./error-displayer-2d.service";
 
 describe("ErrorDisplayer2dService", () => {
@@ -8,5 +11,38 @@ describe("ErrorDisplayer2dService", () => {
   it("should be created", () => {
     const service: ErrorDisplayer2dService = TestBed.get(ErrorDisplayer2dService);
     expect(service).toBeTruthy();
+  });
+
+  it("should change the click permission to false if subscribed to my subject", () => {
+    const service: ErrorDisplayer2dService = new ErrorDisplayer2dService();
+    let clickPermission: boolean = true;
+    service.clickingPermission.subscribe((permission) => clickPermission = permission);
+    service["changeClickingPermission"](false);
+    expect(clickPermission).toBeFalsy();
+  });
+
+  it("should change the click permission to true if subscribed to my subject", () => {
+    const service: ErrorDisplayer2dService = new ErrorDisplayer2dService();
+    let clickPermission: boolean = true;
+    service.clickingPermission.subscribe((permission) => clickPermission = permission);
+    service["changeClickingPermission"](true);
+    expect(clickPermission).toBeTruthy();
+  });
+
+  it("should correctly set the font of the canvas context", () => {
+    const service: ErrorDisplayer2dService = new ErrorDisplayer2dService();
+    const testCanvas: HTMLCanvasElement = document.createElement("canvas");
+    const ctx: CanvasRenderingContext2D = testCanvas.getContext("2d") as CanvasRenderingContext2D;
+    service["setStyle"](ctx);
+    const redColorHex: string = "#ff0000";
+    expect(ctx.font).toEqual(Constants.ERROR_FONT);
+    expect(ctx.fillStyle).toEqual(redColorHex);
+  });
+
+  it("should correctly adjust the position of the error message", () => {
+    const service: ErrorDisplayer2dService = new ErrorDisplayer2dService();
+    const clickPos: IClickInfo = service["getAdjustedErrorPosition"](100, 100);
+    expect(clickPos.xPos).toEqual(25);
+    expect(clickPos.yPos).toEqual(85);
   });
 });
